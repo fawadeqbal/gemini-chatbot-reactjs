266f8ff265d45e53b4fcbef2f4820c58bafe1f66
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2024-05-31 10:13:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -70,7 +70,6 @@ function App() {
   return (
     <div className="chat-container">
       <h2 className='header'>Chat with Gemini</h2>
-      <h3>Fawad Iqbal</h3>
       <div className="message-list-container">
         <ul className="message-list">
           {messages.map((message, index) => (
